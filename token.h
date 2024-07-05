15fe798ab6ffca2f12dd672b045bff4a4572c383
DefFoxPy <66576421+DefFoxPy@users.noreply.github.com>
2024-07-05 14:58:11 -0400

    Update token.h

--- token.h
+++ token.h
@@ -4,12 +4,6 @@ typedef enum
 {
     TOKEN_EOF = 0,
     TOKEN_SELECT = 258,
-    TOKEN_FROM = 259,
-    TOKEN_WHERE = 260,
-    TOKEN_COMMA = 261,
-    TOKEN_SEMICOLON = 262,
-    TOKEN_WILDCARD = 263,
-    TOKEN_IDENTIFIER = 264
 }
 token_t;
 
@@ -18,11 +12,5 @@ inline const char* to_str(token_t t)
     switch (t)
     {
         case TOKEN_SELECT: return "SELECT";
-        case TOKEN_FROM: return "FROM";
-        case TOKEN_WHERE: return "WHERE";
-        case TOKEN_COMMA: return "COMMA";
-        case TOKEN_SEMICOLON: return "SEMICOLON";
-        case TOKEN_WILDCARD: return "WILDCARD";
-        case TOKEN_IDENTIFIER: return "IDENTIFIER";
     }
 }
