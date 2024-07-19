a995a391fac73607ba50303738298265fd1310f0
Oriana <pedrozapalomar.ov07@gmail.com>
2024-07-18 23:00:30 -0400

    Se añadieron tokes especiales, y documento explicativo

--- scanner.flex
+++ scanner.flex
@@ -14,7 +14,15 @@ MOVE       [Mm][Oo][Vv][Ee]
 TURN_LEFT  [Tt][Uu][Rr][Nn][_][Ll][Ee][Ff][Tt]
 TURN_RIGHT [Tt][Uu][Rr][Nn][_][Rr][Ii][Gg][Hh][Tt]
 LOOP       [Ll][Oo][Oo][Pp]
-FINISH     [Ff][Ii][Nn][Ii][Ss][Hh]
+LIGHT_UP   [Ll][Ii][Gg][Hh][Tt][-][Uu][Pp]
+PROCEDURE  [Pp][Rr][Oo][Cc][Ee][Dd][Uu][Ee]
+CALL       [Cc][Aa][Ll][Ll]
+LEFT_PAREN [(]
+RIGHT_PAREN [)]
+LEFT_BRACE [{]
+RIGHT_BRACE [}]
+COMMA      [,]
+EQUAL      [=]
 IF         [Ii][Ff]
 ELSE       [Ee][Ll][Ss][Ee]
 DEF        [Dd][Ee][Ff]
@@ -27,7 +35,15 @@ DEF        [Dd][Ee][Ff]
 {TURN_LEFT}   { return TOKEN_TURN_LEFT; }
 {TURN_RIGHT}  { return TOKEN_TURN_RIGHT; }
 {LOOP}		  { return TOKEN_LOOP; }
-{FINISH}      { return TOKEN_FINISH; }
+{LIGHT_UP}    { return TOKEN_LIGHT_UP; }
+{PROCEDURE}   { return TOKEN_PROCEDURE; }
+{CALL}        { return TOKEN_CALL; }
+{LEFT_PAREN}  { return TOKEN_LEFT_PAREN; }
+{RIGHT_PAREN} { return TOKEN_RIGHT_PAREN; }
+{LEFT_BRACE}  { return TOKEN_LEFT_BRACE; }
+{RIGHT_BRACE} { return TOKEN_RIGHT_BRACE; }
+{COMMA}       { return TOKEN_COMMA; }
+{EQUAL}       { return TOKEN_EQUAL; }
 {IF}          { return TOKEN_IF; }
 {ELSE}        { return TOKEN_ELSE; }
 {IDENTIFIER}  { return TOKEN_IDENTIFIER; }
--- token.h
+++ token.h
@@ -9,9 +9,17 @@ typedef enum
     TOKEN_TURN_LEFT = 261,
     TOKEN_TURN_RIGHT = 262,
     TOKEN_LOOP = 263,
-    TOKEN_FINISH = 264,
-    TOKEN_IF = 265,
-    TOKEN_ELSE = 266
+    TOKEN_LIGHT_UP = 264,
+    TOKEN_PROCEDURE = 265,
+    TOKEN_CALL = 266,
+    TOKEN_LEFT_PAREN = 267,
+    TOKEN_RIGHT_PAREN = 268,
+    TOKEN_LEFT_BRACE = 269,
+    TOKEN_RIGHT_BRACE = 270,
+    TOKEN_COMMA = 271,
+    TOKEN_EQUAL = 272,
+    TOKEN_IF = 273,
+    TOKEN_ELSE = 274
     
 }
 token_t;
@@ -27,7 +35,15 @@ inline const char* to_str(token_t t)
         case TOKEN_TURN_LEFT: return "TURN LEFT";
         case TOKEN_TURN_RIGHT: return "TURN RIGHT";
         case TOKEN_LOOP: return "TOKEN_LOOP";
-        case TOKEN_FINISH: return "TOKEN_FINISH";
+        case TOKEN_LIGHT_UP: return "TOKEN_LIGHT_UP";
+        case TOKEN_PROCEDURE: return "TOKEN_PROCEDURE";
+        case TOKEN_CALL: return "TOKEN_CALL";
+        case TOKEN_LEFT_PAREN: return "TOKEN_LEFT_PAREN";
+        case TOKEN_RIGHT_PAREN: return "TOKEN_RIGHT_PAREN";
+        case TOKEN_LEFT_BRACE: return "TOKEN_LEFT_BRACE";
+        case TOKEN_RIGHT_BRACE: return "TOKEN_RIGHT_BRACE";
+        case TOKEN_COMMA: return "TOKEN_COMMA";
+        case TOKEN_EQUAL: return "TOKEN_EQUAL";
         case TOKEN_IF: return "TOKEN_IF";
         case TOKEN_ELSE: return "TOKEN_ELSE";
     }
